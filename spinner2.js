d396ba18258d80b3146379fcc7cc057bc0e214fd
AlSid604 <alex_sidor97@hotmail.com>
2021-06-29 22:16:15 +0000

    spinner 2 with less code! using a for loop

---
+++ spinner2.js
@@ -0,0 +1,62 @@
+// setTimeout(() => {
+//   process.stdout.write(`\r| BUFFERING  `);
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write(`\r/ BUFFERING   `);
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write(`\r- BUFFERING  `);
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write(`\r\\ BUFFERING   `);
+// }, 700);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write(`\r| BUFFERING   `);
+// }, 1000);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write(`\r/ BUFFERING  `);
+// }, 1200);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write(`\r- BUFFERING  `);
+// }, 1500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write(`\r\\ BUFFERING  `);
+// }, 1700);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write(`\r| DONE  `);
+// }, 2000);
+
+let time = 100;
+
+let spinner = [
+  "\r| BUFFERING",
+  "\r/ BUFFERING",
+  "\r- BUFFERING",
+  "\r\\ BUFFERING",
+  "\r| BUFFERING",
+  "\r/ BUFFERING",
+  "\r- BUFFERING",
+  "\r| DONE....\n   ",
+  "Thanks!",
+];
+
+for (const spin of spinner) {
+  setTimeout(() => {
+    process.stdout.write(spin);
+  }, time);
+  time += 200;
+}
